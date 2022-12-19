68f0d0a1e96405f6aef983cf44146b98b0d25e7f
christopherbowers <bowersc@bu.edu>
2022-12-19 11:55:49 -0500

    Added Mastodon verification to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,14 @@
-import styles from '@styles/footer.module.scss'
+import styles from '@styles/footer.module.scss';
 
 export default function Footer() {
   return (
     <section className={styles.footer}>
       <p>Designed &amp; Built by Christopher Bowers</p>
+      <span style={{ display: 'none' }}>
+        <a rel="me" href="https://webperf.social/@christopherbowers">
+          Mastodon
+        </a>
+      </span>
     </section>
-  )
+  );
 }
